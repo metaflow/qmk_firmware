c1a8e03d213294e6a8f1cf4035bcefee1e231eaf
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2018-04-09 21:23:49 +0000

    media

--- keyboards/whitefox/keymaps/gem/keymap.c
+++ keyboards/whitefox/keymaps/gem/keymap.c
@@ -33,25 +33,25 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `---------------------------------------------------------------'
      */
     [0] = KEYMAP( \
-        KC_ESC,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  KC_INS, \
-        KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,     KC_LBRC, KC_RBRC, KC_BSPC,          KC_DEL, \
-        MO(2),   KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT, KC_NUHS, KC_ENT,           KC_HOME,\
-        KC_LSFT, KC_NUBS, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH, KC_RSFT,          KC_UP,   KC_END,\
-        KC_LCTL, KC_LGUI, KC_LALT,        LT(1, KC_SPACE),                                     KC_RALT,  KC_CAPS, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT \
+        KC_ESC,  KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  KC_INS, \
+        KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,     KC_LBRC, KC_RBRC, KC_BSPC,          KC_DEL, \
+        MO(2),   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT, KC_NUHS, KC_ENT,           KC_HOME,\
+        KC_LSFT, KC_NUBS, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH, KC_RSFT,          KC_UP,   KC_END,\
+        KC_LCTL, KC_LGUI, KC_LALT,         LT(1, KC_SPACE),                                     KC_RALT,  KC_CAPS, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT \
     ),
     [1] = KEYMAP( \
-        ____,    KC_F1,   KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,  KC_F12,  ____,    ____,    ____,\
-        ____,    KC_1,    ____,   ____,   ____,   ____,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_PSCR,  ____,    ____,    ____,             ____,\
-        ____,    ____,    ____,   ____,   ____,   ____,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_RIGHT, ____,    ____,    ____,             ____,\
-        ____,    ____,    ____,   ____,   ____,   ____,   ____,    ____,    ____,    ____,     ____,     ____,    ____,             KC_PGUP, ____,\
-        ____,    ____,    ____,           ____,                                                ____,     ____,    ____,    KC_HOME, KC_PGDN, KC_END\
+        ____,    KC_F1,   KC_F2,   KC_F3,  KC_F4,  KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,  KC_F12,  ____,    ____,    KC_PAUSE,\
+        ____,    KC_1,    ____,    ____,   ____,   ____,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_PSCR,  ____,    ____,    ____,             ____,\
+        ____,    ____,    KC_SLCK, ____,   ____,   ____,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_RIGHT, ____,    ____,    ____,             ____,\
+        ____,    ____,    ____,    ____,   ____,   ____,   ____,    ____,    ____,    ____,     ____,     ____,    ____,             KC_PGUP, ____,\
+        ____,    ____,    ____,            ____,                                                ____,     ____,    ____,    KC_HOME, KC_PGDN, KC_END\
     ),
     [2] = KEYMAP( \
-        ____,    ____,    ____,   ____,   ____,   ____,   ____,    KC_NLCK, KC_PSLS, KC_PAST,  KC_PMNS,  ____,    ____,    ____,    ____,    ____,\
-        ____,    KC_2,    ____,   ____,   ____,   ____,   ____,    KC_P7,   KC_P8,   KC_P9,    KC_PPLS,  ____,    ____,    ____,             ____,\
-        ____,    ____,    ____,   ____,   ____,   ____,   ____,    KC_P4,   KC_P5,   KC_P6,    KC_PENT,  ____,    ____,    ____,             ____,\
-        ____,    ____,    ____,   ____,   ____,   ____,   ____,    KC_P1,   KC_P2,   KC_P3,    ____,     ____,    ____,             ____,    ____,\
-        ____,    ____,    ____,           ____,                                                ____,     ____,    ____,    ____,    ____,    ____\
+        ____,    ____,    ____,    ____,   ____,   ____,   ____,    KC_NLCK, KC_PSLS, KC_PAST,  KC_PMNS,  ____,    ____,    ____,    ____,    ____,\
+        ____,    KC_2,    ____,    ____,   ____,   ____,   ____,    KC_P7,   KC_P8,   KC_P9,    KC_PPLS,  ____,    ____,    ____,             KC_MUTE,\
+        ____,    ____,    ____,    ____,   ____,   ____,   ____,    KC_P4,   KC_P5,   KC_P6,    KC_PENT,  ____,    ____,    KC_MPLY,          ____,\
+        ____,    ____,    ____,    ____,   ____,   ____,   ____,    KC_P0,   KC_P1,   KC_P2,    KC_P3,    ____,    ____,             KC_VOLU, ____,\
+        ____,    ____,    ____,            ____,                                                ____,     ____,    ____,    ____,    KC_VOLD, ____\
     ),
 };
 
