d324fff4cb7827cefa7e9e6d2f8cda67e1f75602
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2018-04-10 16:39:31 +0000

    tap dance

--- keyboards/whitefox/rules.mk
+++ keyboards/whitefox/rules.mk
@@ -31,6 +31,7 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 BACKLIGHT_DRIVER = custom
 VISUALIZER_ENABLE = yes
+TAP_DANCE_ENABLE = yes  # TD macro
 
 LED_DRIVER = is31fl3731c
 LED_WIDTH = 16
