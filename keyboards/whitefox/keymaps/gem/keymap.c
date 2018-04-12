a5693b6ecb4a18c2d2f6d17ed30cad1b6fb42b91
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2018-04-12 21:16:58 +0100

    delete shortcuts and return of caps

--- keyboards/whitefox/keymaps/gem/keymap.c
+++ keyboards/whitefox/keymaps/gem/keymap.c
@@ -17,10 +17,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "whitefox.h"
 
 #define ____ KC_TRNS
-
-enum {
-  TD_ESC = 0
-};
+#define DEL_LN LSFT(KC_DEL)
+#define CAPS2 LT(2, KC_CAPS)
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Layer 0: Default Layer
@@ -37,17 +35,17 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `---------------------------------------------------------------'
      */
     [0] = KEYMAP( \
-        TD(TD_ESC),  KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  KC_INS, \
+        KC_ESC,  KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  KC_INS, \
         KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,     KC_LBRC, KC_RBRC, KC_BSPC,          KC_DEL, \
-        MO(2),   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT, KC_NUHS, KC_ENT,           KC_HOME,\
+        CAPS2,   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT, KC_NUHS, KC_ENT,           KC_HOME,\
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH, KC_RSFT,          KC_UP,   KC_END,\
         KC_LCTL, KC_LGUI, KC_LALT,         LT(1, KC_SPACE),                                     KC_RALT,  KC_CAPS, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT \
     ),
     [1] = KEYMAP( \
-        ____,    KC_F1,   KC_F2,   KC_F3,  KC_F4,  KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,  KC_F12,  ____,    ____,    KC_PAUSE,\
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,  KC_F4,  KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,  KC_F12,  ____,    ____,    KC_PAUSE, \
         ____,    KC_1,    ____,    ____,   ____,   ____,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_PSCR,  ____,    ____,    ____,             ____,\
-        ____,    ____,    KC_SLCK, ____,   ____,   ____,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_RIGHT, ____,    ____,    ____,             ____,\
-        ____,    ____,    ____,    ____,   ____,   ____,   ____,    ____,    ____,    ____,     ____,     ____,    ____,             KC_PGUP, ____,\
+        ____,    ____,    KC_SLCK, DEL_LN, ____,   ____,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_RIGHT, ____,    ____,    ____,             ____,\
+        ____,    ____,    ____,    KC_DEL, ____,   ____,   ____,    ____,    ____,    ____,     ____,     ____,    ____,             KC_PGUP, ____, \
         ____,    ____,    ____,            ____,                                                ____,     ____,    ____,    KC_HOME, KC_PGDN, KC_END\
     ),
     [2] = KEYMAP( \
@@ -61,8 +59,4 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 const uint16_t fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1),
-};
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_ESC]  = ACTION_TAP_DANCE_DOUBLE(KC_GRV, KC_ESC)
 };
