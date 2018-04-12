9d6f6b15f5e996b7927dec0230cb9fd1e2de8175
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2018-04-12 21:16:58 +0100

    delete shortcuts and return of caps

--- keyboards/whitefox/rules.mk
+++ keyboards/whitefox/rules.mk
@@ -34,15 +34,15 @@ BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration
 ## (Note that for BOOTMAGIC on Teensy LC you have to use a custom .ld script.)
 MOUSEKEY_ENABLE = yes	# Mouse keys
 EXTRAKEY_ENABLE = yes	# Audio control and System control
-CONSOLE_ENABLE = yes	# Console for debug
-COMMAND_ENABLE = yes    # Commands for debug and configuration
+#CONSOLE_ENABLE = yes	# Console for debug
+#COMMAND_ENABLE = yes    # Commands for debug and configuration
 #SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes	    # USB Nkey Rollover
 CUSTOM_MATRIX = yes # Custom matrix file
 BACKLIGHT_ENABLE = yes
 BACKLIGHT_DRIVER = custom
 VISUALIZER_ENABLE = yes
-TAP_DANCE_ENABLE = yes  # TD macro
+#TAP_DANCE_ENABLE = yes  # TD macro
 
 # project specific files
 SRC = matrix.c \
