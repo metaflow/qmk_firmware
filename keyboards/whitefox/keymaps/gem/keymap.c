917cd0ff155b3b254601e390e3df3d694f702d90
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2020-05-22 08:54:25 +0200

    add alt as temporary lvl switch for gaming mode

--- keyboards/whitefox/keymaps/gem/keymap.c
+++ keyboards/whitefox/keymaps/gem/keymap.c
@@ -21,6 +21,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define W_FF LCTL(KC_RGHT)
 #define W_BACK LCTL(KC_LEFT)
 #define CAPS2 LT(2, KC_CAPS)
+#define L_DFLT 0
+#define L_GAME 1
+#define L_NAVI 2
+#define L_UTIL 3
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Layer 0: Default Layer
@@ -36,26 +40,33 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |Ctrl|Gui |Alt |         Space    |Alt | Caps |     |Lef|Dow|Rig|
      * `---------------------------------------------------------------'
      */
-    [0] = KEYMAP( \
+    [L_DFLT] = LAYOUT( \
         KC_GESC, KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  KC_INS, \
         KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,     KC_LBRC, KC_RBRC, KC_BSPC,          KC_DEL, \
         CAPS2,   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT, KC_NUHS, KC_ENT,           KC_HOME,\
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH, KC_RSFT,          KC_UP,   KC_END,\
-        KC_LCTL, KC_LGUI, KC_LALT,         LT(1, KC_SPACE),                                     KC_RALT,  KC_CAPS, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT \
+        KC_LCTL, KC_LGUI, KC_LALT,         LT(L_NAVI, KC_SPACE),                                KC_RALT,  DF(L_GAME), KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT \
     ),
-    [1] = KEYMAP( \
+    [L_NAVI] = LAYOUT( \
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,  KC_F4,  KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,  KC_F12,  ____,    ____,    KC_PAUSE, \
         ____,    KC_F13,  W_FF,    KC_F14, KC_F15, KC_F16, KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_PSCR,  ____,    ____,    ____,             ____, \
         ____,    ____,    KC_SLCK, DEL_LN, ____,   ____,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_RIGHT, ____,    ____,    ____,             ____, \
         ____,    ____,    ____,    KC_DEL, ____,   ____,   W_BACK,  ____,    ____,    ____,     ____,     ____,    ____,             KC_PGUP, ____, \
         ____,    ____,    ____,            ____,                                                ____,     ____,    ____,    KC_HOME, KC_PGDN, KC_END \
     ),
-    [2] = KEYMAP( \
+    [L_UTIL] = LAYOUT( \
         ____,    ____,    ____,    ____,   ____,   ____,   ____,    KC_NLCK, KC_PSLS, KC_PAST,  KC_PMNS,  ____,    ____,    ____,    ____,    ____, \
         ____,    KC_2,    ____,    ____,   ____,   ____,   ____,    KC_P7,   KC_P8,   KC_P9,    KC_PPLS,  ____,    ____,    ____,             KC_MUTE, \
         ____,    ____,    ____,    ____,   ____,   ____,   ____,    KC_P4,   KC_P5,   KC_P6,    KC_PENT,  ____,    ____,    KC_MPLY,          ____, \
         ____,    ____,    ____,    ____,   ____,   ____,   ____,    KC_P0,   KC_P1,   KC_P2,    KC_P3,    ____,    ____,             KC_VOLU, ____, \
         ____,    ____,    ____,            ____,                                                ____,     ____,    ____,    ____,    KC_VOLD, ____ \
+    ),
+    [L_GAME] = LAYOUT( \
+        KC_ESC, KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  KC_INS, \
+        KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,     KC_LBRC, KC_RBRC, KC_BSPC,          KC_DEL, \
+        KC_CAPS,   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT, KC_NUHS, KC_ENT,           KC_HOME,\
+        KC_LSFT, KC_NUBS, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH, KC_RSFT,          KC_UP,   KC_END,\
+        KC_LCTL, KC_LGUI, KC_LALT,         KC_SPACE ,                                      LT(L_NAVI, KC_RALT),  DF(L_DFLT), KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT \
     ),
 };
 
