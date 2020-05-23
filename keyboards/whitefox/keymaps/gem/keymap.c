dd6de416ebae6acb9540c3e50f9fec5b2f6308cc
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2020-05-23 07:34:40 +0200

    vol up / down

--- keyboards/whitefox/keymaps/gem/keymap.c
+++ keyboards/whitefox/keymaps/gem/keymap.c
@@ -41,16 +41,16 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `---------------------------------------------------------------'
      */
     [L_DFLT] = LAYOUT( \
-        KC_GESC, KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  KC_INS, \
-        KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,     KC_LBRC, KC_RBRC, KC_BSPC,          KC_DEL, \
-        CAPS2,   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT, KC_NUHS, KC_ENT,           KC_HOME,\
-        KC_LSFT, KC_NUBS, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH, KC_RSFT,          KC_UP,   KC_END,\
-        KC_LCTL, KC_LGUI, KC_LALT,         LT(L_NAVI, KC_SPACE),                                KC_RALT,  DF(L_GAME), KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT \
+        KC_GESC, KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSLS, KC_GRV,  KC_INS, \
+        KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSPC,          KC_DEL, \
+        CAPS2,   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT,    KC_NUHS,  KC_ENT,           KC_HOME,\
+        KC_LSFT, KC_NUBS, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT, KC_UP,   KC_END,\
+        KC_LCTL, KC_LGUI, KC_LALT,         LT(L_NAVI, KC_SPACE),                                KC_RALT,  DF(L_GAME), KC_NO,    KC_LEFT, KC_DOWN, KC_RGHT \
     ),
     [L_NAVI] = LAYOUT( \
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,  KC_F4,  KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,  KC_F12,  ____,    ____,    KC_PAUSE, \
-        ____,    KC_F13,  W_FF,    KC_F14, KC_F15, KC_F16, KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_PSCR,  ____,    ____,    ____,             ____, \
-        ____,    ____,    KC_SLCK, DEL_LN, ____,   ____,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_RIGHT, ____,    ____,    ____,             ____, \
+        ____,    KC_F13,  W_FF,    KC_F14, KC_F15, KC_F16, KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_PSCR,  ____,    ____,    ____,             KC_VOLU, \
+        ____,    ____,    KC_SLCK, DEL_LN, ____,   ____,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_RIGHT, ____,    ____,    ____,             KC_VOLD, \
         ____,    ____,    ____,    KC_DEL, ____,   ____,   W_BACK,  ____,    ____,    ____,     ____,     ____,    ____,             KC_PGUP, ____, \
         ____,    ____,    ____,            ____,                                                ____,     ____,    ____,    KC_HOME, KC_PGDN, KC_END \
     ),
@@ -62,11 +62,11 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ____,    ____,    ____,            ____,                                                ____,     ____,    ____,    ____,    KC_VOLD, ____ \
     ),
     [L_GAME] = LAYOUT( \
-        KC_ESC, KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  KC_INS, \
-        KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,     KC_LBRC, KC_RBRC, KC_BSPC,          KC_DEL, \
-        KC_CAPS,   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT, KC_NUHS, KC_ENT,           KC_HOME,\
-        KC_LSFT, KC_NUBS, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH, KC_RSFT,          KC_UP,   KC_END,\
-        KC_LCTL, KC_LGUI, KC_LALT,         KC_SPACE ,                                      LT(L_NAVI, KC_RALT),  DF(L_DFLT), KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT \
+        KC_ESC,  KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,    KC_7,    KC_8,    KC_9,     KC_0,                KC_MINS,    KC_EQL,    KC_BSLS, KC_GRV,  KC_INS, \
+        KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,                KC_LBRC,    KC_RBRC,   KC_BSPC,          KC_DEL, \
+        KC_CAPS, KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,             KC_QUOT,    KC_NUHS,   KC_ENT,           KC_HOME,\
+        KC_LSFT, KC_NUBS, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,              KC_SLSH,    KC_RSFT,            KC_UP,   KC_END,\
+        KC_LCTL, KC_LGUI, KC_LALT,         KC_SPACE ,                                           LT(L_NAVI, KC_RALT), DF(L_DFLT), KC_NO,     KC_LEFT, KC_DOWN, KC_RGHT \
     ),
 };
 
