31cba7552828d85d6a05dc8973508f7b3aa20e39
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2021-04-28 18:25:27 +0200

    keymap

--- keyboards/ergodox_ez/keymaps/gem/keymap.c
+++ keyboards/ergodox_ez/keymaps/gem/keymap.c
@@ -57,13 +57,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [GAME] = LAYOUT_ergodox_pretty(
   // left hand
   KC_ESC,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_6,                  KC_PAUSE,     KC_6,    KC_7,    KC_8,    KC_9,     KC_0,      KC_BSPC,
-  KC_TAB,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    KC_Y,                  KC_VOLU,     KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,      KC_BSLS,
+  KC_TAB,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    KC_EQL,                KC_VOLU,     KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,      KC_BSLS,
   KC_LCTL,         KC_A,        KC_S,          KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,   KC_QUOT,
-  KC_LSFT,         KC_Z,        KC_X,          KC_C,    KC_V,    KC_B,    KC_N,                  KC_VOLD,     KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,   KC_RSFT,
+  KC_LSFT,         KC_Z,        KC_X,          KC_C,    KC_V,    KC_B,    KC_MINS,               KC_VOLD,     KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,   KC_RSFT,
   KC_LALT,         KC_H,        KC_LBRC,       KC_RBRC,    KC_SPC,                                                        KC_LEFT, KC_DOWN, KC_UP,    KC_RIGHT,  KC_LWIN,
-                                                                 KC_7,    KC_8,                  DF(SC2),     KC_MS_BTN2,
+                                                                 KC_7, TT(MISC),                 DF(BASE),     KC_MS_BTN2,
                                                                           KC_9,                  KC_PSCR,
-                                                           KC_SPC,  KC_J, KC_L,                  KC_MS_BTN1,  MO(SYMB), KC_ENT
+                                                           KC_SPC,  KC_J, KC_L,                  KC_MS_BTN1,  MO(SYMB), LT(SYMB, KC_ENT)
 ),
 [SC2] = LAYOUT_ergodox_pretty(
   // left hand
@@ -84,40 +84,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _____, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_GRV , _____,        KC_PGDN, KC_AMPR, KC_BSPC,  LSFT(KC_DEL),  KC_DEL, _____, _____,
   _____,   _____, _____, _____, _____,                                         _____,     _____,   _____,  _____,  _____,
                                           _____, DF(BASE),     _____, _____,
-                                                 DF(GAME),      _____,
+                                                 DF(GAME),      _____,/*  */
                                       _____, _____, _____,        _____, _____, _____
 ),
-/* Keymap 2: Media and mouse keys
- *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      | MsUp |      |      |      |           |      |      |      |      |      |      |        |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |MsLeft|MsDown|MsRght|      |------|           |------|      |      |      |      |      |  Play  |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      | Prev | Next |      |        |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      | Lclk | Rclk |                                       |VolUp |VolDn | Mute |      |      |
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        |      |      |       |      |      |
- *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |      |       |      |      |Brwser|
- *                                 |      |      |------|       |------|      |Back  |
- *                                 |      |      |      |       |      |      |      |
- *                                 `--------------------'       `--------------------'
- */
 [MISC] = LAYOUT_ergodox_pretty(
-  // left hand
-  _____, _____, _____, _____, _____, _____, _____,        _____,   _____, _____, _____, _____, _____, KC_BSPC,
+  // left hand/*  */
+  _____, _____, _____, S(A(KC_3)), S(A(KC_4)), S(A(KC_5)), _____,          _____,   _____, _____, _____, _____, _____, KC_BSPC,
   _____, _____,   _____,   KC_MS_U, _____,   _____, KC_VOLU,      KC_VOLU, _____, KC_7, KC_8,   KC_9,  _____, _____,
-  KC_CAPS, _____, KC_MS_L, KC_MS_D, KC_MS_R, _____,                _____, KC_4, KC_5,   KC_6,  KC_MPLY, _____,
-  _____, _____, _____, _____, _____, _____, KC_VOLD,        KC_VOLD, _____, KC_1, KC_2,   KC_3, _____, _____,
+  KC_CAPS, _____, KC_MS_L, KC_MS_D, KC_MS_R, _____,               _____, KC_4, KC_5,   KC_6,  KC_MPLY, _____,
+  _____, _____, _____, _____, _____, _____, KC_VOLD,              KC_VOLD, _____, KC_1, KC_2,   KC_3, _____, _____,
   RESET, _____, _____, KC_BTN1, KC_BTN2,                                  KC_0, KC_DOT, _____, _____, _____,
 
-                                               _____, _____,     _____, _____,
-                                                        _____,     _____,
+                                               _____, _____,     DF(SC2), _____,
+                                                    _____,     _____,
                                       _____, _____, _____,     _____, _____, _____
 ),
 };
